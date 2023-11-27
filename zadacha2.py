52bd788a0076b8e5a5bb175c9b1c1af818480796
Mariia Abramova <149184746+willwolker@users.noreply.github.com>
2023-11-27 18:27:30 +0300

    Add files via upload

---
+++ zadacha2.py
@@ -0,0 +1,12 @@
+from drawbot_skia.drawbot import rect, fill, strokeWidth, saveimage
+fill(None)
+strokeWidth (10)
+rect (50, 100, 200, 200)
+x = 50
+y = 50
+step = 100
+for i in range(15):
+rect(x, y, 200, 100)
+x = x + step
+y = y + step 
+saveimage ('zadacha2.pdf')
